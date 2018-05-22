b5883f158cfae2dab5b557ab0c9fa4bcc5db09bd
WIlliam Calderon <wec999@gmail.com>
2018-05-23 00:28:07 +0800

    Change coin model, route modified

--- db/migrate/20180522162204_create_coins.rb
+++ db/migrate/20180522162204_create_coins.rb
@@ -2,7 +2,6 @@ class CreateCoins < ActiveRecord::Migration[5.2]
   def change
     create_table :coins do |t|
       t.string :name
-      t.decimal :price
       t.integer :quantity
       t.references :user, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,48 +15,13 @@ ActiveRecord::Schema.define(version: 2018_05_22_162204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bitcoin_payment_transactions", force: :cascade do |t|
-    t.integer "estimated_value"
-    t.string "transaction_hash"
-    t.string "block_hash"
-    t.datetime "block_time"
-    t.datetime "estimated_time"
-    t.integer "bitcoin_payment_id"
-    t.integer "btc_conversion"
-    t.integer "confirmations"
-    t.index ["bitcoin_payment_id"], name: "index_bitcoin_payment_transactions_on_bitcoin_payment_id"
-  end
-
-  create_table "bitcoin_payments", force: :cascade do |t|
-    t.string "payable_type"
-    t.integer "payable_id"
-    t.string "currency"
-    t.string "reason"
-    t.integer "price"
-    t.float "btc_amount_due", default: 0.0
-    t.string "address"
-    t.string "state"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "btc_conversion"
-    t.index ["payable_type", "payable_id"], name: "index_bitcoin_payments_on_payable_type_and_payable_id"
-  end
-
   create_table "coins", force: :cascade do |t|
     t.string "name"
-    t.decimal "price"
     t.integer "quantity"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_coins_on_user_id"
-  end
-
-  create_table "currency_conversions", force: :cascade do |t|
-    t.float "currency"
-    t.integer "btc"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
