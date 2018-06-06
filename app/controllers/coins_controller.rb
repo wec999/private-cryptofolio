8c5fc41bf87ac1619738162097ace713e8404581
WIlliam Calderon <wec999@gmail.com>
2018-06-06 23:50:56 +0800

    CRUD for coins created

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,23 +1,53 @@
 class CoinsController < ApplicationController
+  before_action :set_coin, only: [:show, :edit, :update, :destroy]
+
   def new
     @coin = Coin.new
   end
 
   def create
+    @coin = Coin.new(coin_params)
+    @coin.user = current_user
+    if @coin.save!
+      redirect_to coin_path(@coin)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @coin.update(coin_params)
+      redirect_to coin_path(@coin)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @coin.destroy
+    redirect_to coins_path
   end
 
   def index
+    @coins = Coin.all
   end
 
   def show
+    listing_api_coinmarketcap
   end
+
+  private
+
+  def set_coin
+    @coin = Coin.find(params[:id])
+  end
+
+  def coin_params
+    params.require(:coin).permit(:name,:quantity)
+  end
+
+
 end
--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,3 +1,10 @@
 class Coin < ApplicationRecord
   belongs_to :user
+
+  def listing_api_coinmarketcap
+    url = "https://api.coinmarketcap.com/v2/listings/"
+    response = RestClient.get(url)
+    @data = JSON.parse (response)
+  end
+
 end
